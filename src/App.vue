6c2267de0e7a45f012a7981f6c0c40a286c6a095
Paul Halliday <paulhalliday@pauls-imac.home>
2017-12-04 11:35:39 +0000

    Completion

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,46 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div>
+    <navigation-bar />
+    <div class="container">
+      <message-list :messages="messages" />
+      <message-form />
+    </div>
+    <connection-status :isConnected="isConnected" />
   </div>
 </template>
 
 <script>
+import MessageForm from './components/MessageForm.vue';
+import MessageList from './components/MessageList.vue';
+
 export default {
-  name: 'app',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+      isConnected: false,
+      messages: [],
+    };
+  },
+  components: {
+    MessageList,
+    MessageForm,
+  },
+  sockets: {
+    connect() {
+      this.isConnected = true;
+    },
+    disconnect() {
+      this.isConnected = false;
+    },
+    chatMessage(messages) {
+      this.messages = messages;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.container {
+  width: 300px;
+  margin: 0 auto;
 }
 </style>
